58f068072eaaf0b7530b817be744a3b0238b440c
Edwin Avila <edwinfabricioavilacueva@gmail.com>
2019-05-17 17:59:17 -0500

    cambios de angular formularios

---
+++ 04-Formularios/formularios/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'formularios';
+  
+  nombre: string;
+  apellido: string;
+  q: string;
+
+  guardarUsuario(formulario){
+    console.log(formulario);
+    this.nombre = "Benito C."
+    this.apellido = "Bernal"
+    this.q = "musica"  
+  }
+}
