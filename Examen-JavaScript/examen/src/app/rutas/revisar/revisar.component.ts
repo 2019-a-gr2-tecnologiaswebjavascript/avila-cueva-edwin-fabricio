ea081d6971bad6b1b41e337eb76d7a76e625d2e9
Edwin Avila <edwinfabricioavilacueva@gmail.com>
2019-06-03 15:45:16 -0500

    cambio 10

--- Examen-JavaScript/examen/src/app/rutas/revisar/revisar.component.ts
+++ Examen-JavaScript/examen/src/app/rutas/revisar/revisar.component.ts
@@ -11,8 +11,32 @@ export class RevisarComponent implements OnInit {
   arregloFacturas;
   constructor(private readonly _facturaService:FacturaService) { }
 
+  nombreABuscar;
+  arregloBusquedaCajero=[];
+  arregloBusquedaUsuario=[];
+  esconder=false;
+  ver=false;
+
   ngOnInit() {
     this.arregloFacturas = this._facturaService.arregloFacturas;
+  }
+
+  buscarNombre(evento){
+    this.esconder=evento;
+    this.arregloFacturas.find(
+      (valor)=>{
+        if(valor.nombreCajeroFact === this.nombreABuscar){
+          this.arregloBusquedaCajero.push(valor);
+          console.log('arreglo cajero',this.arregloBusquedaCajero);
+          return this.arregloBusquedaCajero
+        }else if(valor.nombreComprar === this.nombreABuscar){
+          this.arregloBusquedaUsuario.push(valor);
+          console.log('arreglo Usuaoir',this.arregloBusquedaCajero);
+          this.ver=evento;
+          return this.arregloBusquedaUsuario;
+        }
+      }
+    );
   }
 
 }
